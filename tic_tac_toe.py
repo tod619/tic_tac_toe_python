3addfa2e10b9c7107464737fc2d835a9fa17104a
tod619 <trevorodwyer619@hotmail.com>
2023-05-17 19:20:13 +0100

    Create global constants for use in the game

---
+++ tic_tac_toe.py
@@ -0,0 +1,13 @@
+# Tic Tac Toe
+# A game of Tic Tac Toe against a computer based oppenent
+# The game uses a simple A.I model to pick counter moves against a human oppenent
+# 17/05/2023
+
+# Global Constants
+X = "X"
+O = "O"
+EMPTY = " "
+TIE = "TIE"
+NUM_SQUARES = 9
+
+input("\nPress Enter to exit the program.")
